d186c720866b2bf1927da145d2ea166198ee0bcd
Sendila Jayasundara <sendilajayasundara@gmail.com>
2026-03-15 23:31:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,10 +28,3 @@ ScrollReveal().reveal('.hero-text',{delay:200});
 ScrollReveal().reveal('.hero-image',{delay:400});
 ScrollReveal().reveal('.skill-card',{interval:200});
 ScrollReveal().reveal('.project-card',{interval:200});
-
-const menuToggle = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
-
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
